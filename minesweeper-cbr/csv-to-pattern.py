cfbe300b33978507d562aceb6a54404290c2a2cb
marcel-noehre <marcelnoehre@web.de>
2022-09-25 11:45:01 +0200

    add script starting points

--- minesweeper-cbr/csv-to-pattern.py
+++ minesweeper-cbr/csv-to-pattern.py
@@ -1,4 +1,8 @@
-csv = input()
-pattern = csv.replace('"', '')
-print(pattern.replace(',', ''))
-input()
+def main(): 
+    csv = input()
+    pattern = csv.replace('"', '')
+    print(pattern.replace(',', ''))
+    input()
+
+if __name__ == '__main__':
+    main()
--- minesweeper-cbr/pattern-to-csv.py
+++ minesweeper-cbr/pattern-to-csv.py
@@ -1,6 +1,10 @@
-pattern = input()
-csv = ''
-for char in pattern:
-    csv += '"' + char + '",'
-print(csv[:-1])
-input()
+def main():
+    pattern = input()
+    csv = ''
+    for char in pattern:
+        csv += '"' + char + '",'
+    print(csv[:-1])
+    input()
+
+if __name__ == '__main__':
+    main()
