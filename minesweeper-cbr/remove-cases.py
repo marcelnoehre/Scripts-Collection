b74bd8a51fc135947065f6937b1911babda8acfa
marcel-noehre <marcelnoehre@web.de>
2022-09-28 19:36:03 +0200

    remove unsolveable cases script

---
+++ minesweeper-cbr/remove-cases.py
@@ -0,0 +1,14 @@
+from csv import QUOTE_ALL, reader, writer
+
+
+def main(): 
+    with open('C:/Users/Marcel/cbr-workspace/Minesweeper-CBR-Backend/src/main/webapp/WEB-INF/resources/CaseBase.csv') as file:
+        data = list(filter(lambda x: 'True' in x, [row for row in file]))
+    with open('C:/Users/Marcel/cbr-workspace/Minesweeper-CBR-Backend/src/main/webapp/WEB-INF/resources/CaseBase.csv', 'a') as file:
+        file.seek(0)
+        file.truncate()
+        for row in data:
+            file.write(row)
+
+if __name__ == '__main__':
+    main()
