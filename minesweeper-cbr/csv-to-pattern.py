535c775f6a7b3261e5196cc8e178bafad44030ed
marcel-noehre <marcelnoehre@web.de>
2022-09-29 14:35:08 +0200

    remove redundant cases from casebase

--- minesweeper-cbr/csv-to-pattern.py
+++ minesweeper-cbr/csv-to-pattern.py
@@ -1,5 +1,5 @@
 def main(): 
-    print(input().replace('"', '').replace(',', ''))
+    print(input().replace('"', '').replace("'", '').replace(',', '').replace(' ', ''))
 
 if __name__ == '__main__':
     main()
